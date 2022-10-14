e92d55f0f5149a980d039793981a3c97d3840424
Jade Stewart <jadekstewart3@gmail.com>
2022-10-14 17:43:10 -0600

    Reassign string variables

--- variables.rb
+++ variables.rb
@@ -2,6 +2,14 @@ string_1 = "My name is Jade."
 string_2 = "I like to rock climb."
 string_3 = "My pets are cute."
 
+string_1 = "Hi, I'm Paul."
+string_2 = "Paul is a 3 eyed monkey."
+string_3 = "We all Paul."
+
+puts string_1
+puts string_2
+puts string_3
+
 int_1 = 1111
 int_2 = 2222
 int_3 = 3333
